aa9659b4212dc87e7bfc3d4056d8628f27960320
easysir <easysir10@163.com>
2021-08-19 19:26:18 +0800

    增加生产者

---
+++ src/main/java/KafkaKerberosConsumer.java
@@ -0,0 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * kafka使用kerberos认证-数据消费
+ *
+ * @author hwang
+ * @data 2021/8/19
+ */
+public class KafkaKerberosConsumer {
+    /**
+     * 日志对象
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaKerberosConsumer.class);
+
+    private static final long COUNT = 10;
+}
--- src/main/java/KafkaKerberosProducer.java
+++ src/main/java/KafkaKerberosProducer.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
-import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -18,37 +17,32 @@ public class KafkaKerberosProducer {
      * 日志对象
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaKerberosProducer.class);
-    /**
-     * kerberos配置文件
-     */
-    public static final String KRB5_CONF = "krb5.conf";
 
-    public static final String KAFKA_JAAS_CONF = "kafka-jaas.conf";
-    public static final String BOOTSTRAP_SERVERS = "server:9092";
-    public static final String TOPIC = "test";
-    private static final long COUNT = 5;
+    private static final long COUNT = 10;
 
     public static void main(String[] args) {
-        // Kerberos认证必须添加
-        System.setProperty("java.security.krb5.conf", KRB5_CONF);
-        System.setProperty("java.security.auth.login.config", KAFKA_JAAS_CONF);
+        // Kerberos认证所需配置
+        System.setProperty("java.security.krb5.conf", "D:\\myWorkspace\\kafka-kerberos-study\\src\\main\\resources\\krb5.conf");
+        System.setProperty("java.security.auth.login.config", "D:\\myWorkspace\\kafka-kerberos-study\\src\\main\\resources\\kafka-jaas.conf");
 
+        // kafka相关配置
         Properties props = new Properties();
-        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        props.put("bootstrap.servers", "1.15.154.243:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         // Kerberos认证必须添加以下三行
         props.put("security.protocol", "SASL_PLAINTEXT");
         props.put("sasl.kerberos.service.name", "kafka");
         props.put("sasl.mechanism", "GSSAPI");
+
+        // 新建生产者
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
         int i = 1;
-
         do {
-            String message = "{\"id\":" + i + ",\"date\":" + new Date() + "}";
+            String message = "{\"id\":" + i + ",\"date" + i + "\":" + new Date() + "}";
             LOGGER.info("推送数据:{}", message);
             // 推送数据
-            producer.send(new ProducerRecord<>(TOPIC, message));
+            producer.send(new ProducerRecord<>("test", message));
         } while (i++ <= COUNT);
     }
 }
